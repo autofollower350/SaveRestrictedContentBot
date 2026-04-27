f4c6ff5015b64f529402d41d8184fe816e7ca469
autofollower350 <followerauto350@gmail.com>
2026-04-27 08:50:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,5 @@
 API_ID = 28590286            # Yahan apna number likho
 API_HASH = "6a68cc6b41219dc57b7a52914032f92f" # Yahan quotes ke andar hash likho
 BOT_TOKEN = "7936101320:AAEB2zvfFoNTsU82mnPKsE6a9DvH4dtshXY"
-SESSION = "yahan_string_session_likho"
 AUTH = 6117445553             # Apni user ID likho
 FORCESUB = None # Channel ka username (bina @ ke)
