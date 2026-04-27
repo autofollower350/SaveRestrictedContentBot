f1d1c8a9d7bcbae7fe069b8bf6e6b34199f96662
autofollower350 <followerauto350@gmail.com>
2026-04-27 09:04:00 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-API_ID = 28590286            # Yahan apna number likho
-API_HASH = "6a68cc6b41219dc57b7a52914032f92f" # Yahan quotes ke andar hash likho
-BOT_TOKEN = "7936101320:AAEB2zvfFoNTsU82mnPKsE6a9DvH4dtshXY"
-AUTH = 6117445553             # Apni user ID likho
-FORCESUB = None # Channel ka username (bina @ ke)
